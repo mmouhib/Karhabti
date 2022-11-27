a16d9830663fa43a9a703728c0c24267efc3c4e8
mouhib <iwritebadcode@github.com>
2022-11-27 16:35:34 +0100

    Add react-router & configure routes.

--- Karhabti.WebClient/Karhabti/src/App.tsx
+++ Karhabti.WebClient/Karhabti/src/App.tsx
@@ -1,11 +1,24 @@
 import Home from './pages/home';
 import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/signup',
+		element: <Signup />,
+	},
+]);
 
 export default function App() {
-	return (
-		<>
-			{/*<Home />*/}
-			<Login />
-		</>
-	);
+	return <RouterProvider router={router} />;
 }
