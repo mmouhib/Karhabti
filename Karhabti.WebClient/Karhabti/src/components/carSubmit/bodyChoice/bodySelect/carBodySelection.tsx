25d9a64287326898d1ffe3a05496776356322c21
mouhib <iwritebadcode@github.com>
2022-11-29 03:03:21 +0100

    Change car body types icons.

--- Karhabti.WebClient/Karhabti/src/components/carSubmit/bodyChoice/bodySelect/carBodySelection.tsx
+++ Karhabti.WebClient/Karhabti/src/components/carSubmit/bodyChoice/bodySelect/carBodySelection.tsx
@@ -1,22 +1,30 @@
 import './carBodySelection.scss';
-import Commercial from '../../../../assets/carBodyType/commercial.png';
+
 import Coupe from '../../../../assets/carBodyType/coupe.png';
 import Pickup from '../../../../assets/carBodyType/pickup.png';
+import Hatchback from '../../../../assets/carBodyType/hatchback.png';
 import Roadster from '../../../../assets/carBodyType/roadster.png';
 import Sedan from '../../../../assets/carBodyType/sedan.png';
 import Suv from '../../../../assets/carBodyType/suv.png';
+import Wagon from '../../../../assets/carBodyType/wagon.png';
+
 import Item from '../item/item';
 import { useState } from 'react';
 
 export default function CarBodySelecton() {
-	const imageList: any[] = [Coupe, Pickup, Roadster, Suv];
+	const imageList: any[] = [
+		Coupe,
+		Pickup,
+		Hatchback,
+		Roadster,
+		Sedan,
+		Suv,
+		Wagon,
+	];
 
-	const [selectionList, setSelectionList] = useState<boolean[]>([
-		false,
-		true,
-		false,
-		false,
-	]);
+	const [selectionList, setSelectionList] = useState<boolean[]>(
+		new Array(imageList.length).fill(false)
+	);
 
 	return (
 		<div className="car-body-container">
