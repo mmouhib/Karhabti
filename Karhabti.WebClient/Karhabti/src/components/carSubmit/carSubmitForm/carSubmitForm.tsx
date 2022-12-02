cde1297ddff7042d01b8f088e38ca14598ccfdb1
mouhib <iwritebadcode@github.com>
2022-12-03 00:04:42 +0100

    Start car info form.

--- Karhabti.WebClient/Karhabti/src/components/carSubmit/carSubmitForm/carSubmitForm.tsx
+++ Karhabti.WebClient/Karhabti/src/components/carSubmit/carSubmitForm/carSubmitForm.tsx
@@ -1,21 +1,26 @@
 import './carSubmitForm.scss';
+import AddCarImage from '../../../assets/addCar.png';
 
 interface ICarBodySelectonProps {
 	setCurrentComponent: (arg: boolean) => void;
 }
 
 export default function CarSubmitForm(props: ICarBodySelectonProps) {
+	const changeComponent = (): void => {
+		props.setCurrentComponent(true);
+	};
+
 	return (
 		<div className="car-submit-form-container">
-			<div className="left"></div>
+			<div className="left">
+				<div className="text">
+					<h1>Add your car</h1>
+					<p>Fill in your car informations</p>
+				</div>
+				<img src={AddCarImage} alt="AddCarImage" />
+			</div>
 			<div className="right">
-				<p
-					onClick={() => {
-						props.setCurrentComponent(true);
-					}}
-				>
-					aaaaa
-				</p>
+				<p onClick={changeComponent}></p>
 			</div>
 		</div>
 	);
