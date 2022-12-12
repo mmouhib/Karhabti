558ca405924123edabf3f27642b29cb209adc034
mouhib <iwritebadcode@github.com>
2022-12-12 12:55:08 +0100

    Add profile logout.

--- Karhabti.WebClient/Karhabti/src/components/profile/nav/nav.tsx
+++ Karhabti.WebClient/Karhabti/src/components/profile/nav/nav.tsx
@@ -1,11 +1,21 @@
 import './nav.scss';
 import Logo from '../../_global/logo/logo';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProfileNav() {
+	const navigate = useNavigate();
 	return (
 		<nav className="profile-nav-container">
 			<Logo />
-			<p>Logout</p>
+			<p
+				className="logout"
+				onClick={() => {
+					localStorage.clear();
+					navigate('/');
+				}}
+			>
+				Logout
+			</p>
 		</nav>
 	);
 }
